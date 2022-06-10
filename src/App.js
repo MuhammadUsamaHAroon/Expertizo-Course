43a344016d4c7dfa58d7ecf63758747d3248854f
Muhammad Usama Haroon <muhammadusamaharoon13579@gmail.com>
2022-06-10 00:37:30 -0700

    Assigment-1

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Burger from "../src/Assets/Images/cheezburger.JPG";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table, Container } from "react-bootstrap";
+function App() {
+  const txt = "Hello World";
+
+  const obj = {
+    naam: "Hello World Object",
+  };
+
+  const arr = ["We", "are", "United"];
+
+  const arrObj = [
+    { name: "Hello World 1" },
+    { name: "Hello World 2" },
+    { name: "Hello World 3" },
+  ];
+
+  const complex = [
+    { company: "XYZ", jobs: ["JavaScript", "React"] },
+    { company: "ABC", jobs: ["Angular", "Ionic"] },
+  ];
+  return (
+    <div className="App">
+      {/* TASK 1  */}
+
+      <h1>Task 1: const name = "Hello World";</h1>
+      <p>
+        <b>{txt}</b>
+      </p>
+      <hr />
+
+      {/* TASK 2 */}
+
+      <h1>`Task 2: const object = naam: "Hello World Object"`</h1>
+      <p>
+        <b>{obj.naam}</b>
+      </p>
+      <hr />
+
+      {/* TASK 3 */}
+
+      <h1>
+        Task 3: const data = ['We', 'are', 'United'] //Show these in separate
+        tags
+      </h1>
+      <ol style={{ width: "5%", margin: "auto" }}>
+        <b>
+          {" "}
+          <li>{arr[0]}</li>
+        </b>
+        <b>
+          {" "}
+          <li>{arr[1]}</li>
+        </b>
+        <b>
+          {" "}
+          <li>{arr[2]}</li>
+        </b>
+      </ol>
+      <hr />
+
+      {/* TASK 4 */}
+
+      <h1>Task 4:Write Array Object //Show these in separate tags</h1>
+      <ul style={{ width: "10%", margin: "auto" }}>
+        <b>
+          <li>{arrObj[0].name}</li>
+        </b>
+        <b>
+          <li>{arrObj[1].name}</li>
+        </b>
+        <b>
+          <li>{arrObj[2].name}</li>
+        </b>
+      </ul>
+      <hr />
+
+      {/* TASK 5 */}
+
+      <h1>Task 5: Write Array Object //Show these in a Table</h1><br/>
+      <Container>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Company</th>
+              <th>Jobs</th>
+              <th>Jobs</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>{complex[0].company}</td>
+              <td>{complex[0].jobs[0]}</td>
+              <td>{complex[0].jobs[1]}</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>{complex[1].company}</td>
+              <td>{complex[1].jobs[0]}</td>
+              <td>{complex[1].jobs[1]}</td>
+            </tr>
+            
+          </tbody>
+        </Table>
+        </Container>
+      
+    </div>
+  );
+}
+
+export default App;
