0f44a840d82067a6fbccbef7809de0ea73f0de22
Muhammad Usama Haroon <muhammadusamaharoon13579@gmail.com>
2022-06-10 11:33:21 -0700

    Changes in Assigment-1

--- src/App.js
+++ src/App.js
@@ -3,37 +3,46 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Table, Container } from "react-bootstrap";
 
 function App() {
-
-  {/* TASK 1  */}
+  {
+    /* TASK 1  */
+  }
 
   const txt = "Hello World";
   console.log(txt);
 
-{/* TASK 2  */}
+  {
+    /* TASK 2  */
+  }
 
   const obj = {
     naam: "Hello World Object",
   };
   console.log(obj.naamnaam);
 
-  {/* TASK 3  */}
+  {
+    /* TASK 3  */
+  }
 
   const arr = ["We", "are", "United"];
   console.log(arr[0] + " " + arr[1] + " " + arr[2]);
 
-  {/* TASK 4  */}
+  {
+    /* TASK 4  */
+  }
 
   const arrObj = [
-    { name: "Hello World 1" },
-    { name: "Hello World 2" },
-    { name: "Hello World 3" },
+    { name: "Hello World 1", id: 0 },
+    { name: "Hello World 2", id: 1 },
+    { name: "Hello World 3", id: 2 },
   ];
 
-  {/* TASK 5  */}
-  
+  {
+    /* TASK 5  */
+  }
+
   const complex = [
-    { company: "XYZ", jobs: ["JavaScript", "React"] },
-    { company: "ABC", jobs: ["Angular", "Ionic"] },
+    { id: 0, company: "XYZ", jobs: ["JavaScript", "React"] },
+    { id: 1, company: "ABC", jobs: ["Angular", "Ionic"] },
   ];
   return (
     <div className="App">
@@ -59,39 +68,35 @@ function App() {
         Task 3: const data = ['We', 'are', 'United'] //Show these in separate
         tags
       </h1>
-      <ol style={{ width: "5%", margin: "auto" }}>
-        <b>
-          <li>{arr[0]}</li>
-        </b>
-        <b>
-          <li>{arr[1]}</li>
-        </b>
-        <b>
-          <li>{arr[2]}</li>
-        </b>
-      </ol>
+      {arr.map((items, index) => {
+        return (
+          <ul style={{ width: "5%", margin: "auto" }} key={index}>
+            <b>
+              <li>{items}</li>
+            </b>
+          </ul>
+        );
+      })}
       <hr />
 
       {/* TASK 4 */}
 
       <h1>Task 4:Write Array Object //Show these in separate tags</h1>
-      <ul style={{ width: "10%", margin: "auto" }}>
-        <b>
-          <li>{arrObj[0].name}</li>
-        </b>
-        <b>
-          <li>{arrObj[1].name}</li>
-        </b>
-        <b>
-          <li>{arrObj[2].name}</li>
-        </b>
-      </ul>
+      {arrObj.map((items) => {
+        return (
+          <ul style={{ width: "10%", margin: "auto" }} key={items.id}>
+            <b>
+              <li>{items.name}</li>
+            </b>
+          </ul>
+        );
+      })}
       <hr />
 
       {/* TASK 5 */}
-
       <h1>Task 5: Write Array Object //Show these in a Table</h1>
-      <br /><br/>
+      <br />
+      <br />
       <Container>
         <Table striped bordered hover variant="dark">
           <thead>
@@ -102,20 +107,18 @@ function App() {
               <th>Jobs</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>{complex[0].company}</td>
-              <td>{complex[0].jobs[0]}</td>
-              <td>{complex[0].jobs[1]}</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>{complex[1].company}</td>
-              <td>{complex[1].jobs[0]}</td>
-              <td>{complex[1].jobs[1]}</td>
-            </tr>
-          </tbody>
+          {complex.map((items, index) => {
+            return (
+              <tbody key={items.id}>
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{items.company}</td>
+                  <td>{items.jobs.slice(1, 3)}</td>
+                  <td>{items.jobs.slice(0, 1)}</td>
+                </tr>
+              </tbody>
+            );
+          })}
         </Table><br/><br/>
       </Container>
     </div>
