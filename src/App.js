f7b928810a45ab4b9921974a92eb804dd356728d
Muhammad Usama Haroon <muhammadusamaharoon13579@gmail.com>
2022-07-02 11:34:29 -0700

    Class-Task

--- src/App.js
+++ src/App.js
@@ -2,126 +2,37 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Col, Row } from "react-bootstrap";
 import { useState } from "react";
-import NavBar from "../src/Components/Navbar/navBar";
-import Question from "./views/Question";
-import ScoreCard from "./views/ScoreCard";
-import Initially from "./views/Initially";
-// import SweetAlert from "react-bootstrap-sweetalert";
+import FbFeed from '../src/views/Fb Feed';
+import Profile from '../src/Assets/Images/Photo.jpg'
+import ImageOne from '../src/Assets/Images/imageOne.jpg';
+import ImageTwo from '../src/Assets/Images/imageTwo.jpg';
+import ImageThree from '../src/Assets/Images/imageThree.jpg';
+import ImageFour from '../src/Assets/Images/imageFour.jpeg';
+import ImageFive from '../src/Assets/Images/imageFive.jpg';
+import ImageSix from '../src/Assets/Images/imageSix.jpg';
+import ImageSeven from '../src/Assets/Images/imageSeven.jpg';
 function App() {
-  const quiz = [
-    {
-      question: "HTML stands for?",
-      option: [
-        { optionAnswer: "(B) Hyper Txt Makeup Language", isCorrect: false },
-        { optionAnswer: "(A) Hyper Text Markup Language", isCorrect: true },
-        { optionAnswer: "(C) Hydro Text Markup Lanugage", isCorrect: false },
-      ],
-    },
-    {
-      question: "what is CSS?",
-      option: [
-        { optionAnswer: "(B) Cdsak", isCorrect: false },
-        { optionAnswer: "(C) Cdsak", isCorrect: false },
-        { optionAnswer: "(A) Cascading Style sheet", isCorrect: true },
-      ],
-    },
-    {
-      question: "How do you call a function named 'myFunction'?",
-      option: [
-        { optionAnswer: "(B) call function myFunction()", isCorrect: false },
-        { optionAnswer: "(A) myFunction()", isCorrect: true },
-        { optionAnswer: "(C) call myFunction()", isCorrect: false },
-      ],
-    },
-    {
-      question:
-        "How to write an IF statement for executing some code if 'i' is NOT equal to ?",
-      option: [
-        { optionAnswer: "(B) if i = !5 then", isCorrect: false },
-        { optionAnswer: "(A) if(i != 5)", isCorrect: true },
-        { optionAnswer: "(C) if (i <> 5) ", isCorrect: false },
-      ],
-    },
-    {
-      question: "How does a WHILE loop start?",
-      option: [
-        { optionAnswer: "(B) while(i <=10; i++)", isCorrect: false },
-        { optionAnswer: "(C) while(i = 1 to 10) ", isCorrect: false },
-        { optionAnswer: "(A) while(i <= 10)", isCorrect: true },
-      ],
-    },
-  ];
-  const [nextQues, setNextQues] = useState(0);
-  const [score, setScore] = useState(0);
-  const [screen, setScreen] = useState("Start");
-  const [submit, setSubmit] = useState(false);
 
-  const question = quiz[nextQues].question;
-  const option = quiz[nextQues].option;
+  const images = [
+    ImageOne,
+    ImageTwo,
+    ImageThree,
+    ImageFour,
+    ImageFive,
+    ImageSix,
+    ImageSeven
 
-  // console.log(question+' '+option)
-  console.log(score);
-
-  const start = ()=>{
-    setScreen('Question')
+  ]
+  const profileDetails = {
+    profileImage:Profile,
+    profileName:'Usama haroon',
+    date: new Date(Date.now()).toLocaleString(),
+    userCaption:'all things are difficult before they become easy.',
   }
-  const quizHandler = (optionValue) => {
-    const optionVal = option.find((items) => {
-      return items.optionAnswer === optionValue;
-    });
-    console.log(optionVal.isCorrect);
-    if (optionVal.isCorrect === true) {
-      setScore(score + 5);
-    } else {
-      console.log(false);
-    }
-    const nextQuestion = nextQues + 1;
-    if (nextQuestion < quiz.length) {
-      setNextQues(nextQuestion);
-    } else {
-      setSubmit(true);
-    }
-  };
-  const previousQuestion = () => {
-    const preQues = nextQues - 1;
-    if (nextQues > 0) {
-      setNextQues(preQues);
-      setSubmit(false);
-    } else {
-      alert("jjkskjdj");
-    }
-    if (nextQues > 0 && nextQues < 4) {
-      setScore(score - 5);
-    } else {
-      setScore(score - 10);
-    }
-  };
-  const submitQuiz = () => {
-    setScreen("ScoreCard");
-  };
-  const reQuiz = () => {
-    setNextQues(0);
-    setScreen("Question");
-    setSubmit(false);
-    setScore(null);
-  };
-
   return (
     <div className="App">
       <Container fluid>
-        {screen === "Question" && (
-          <Question
-            nextQues={nextQues}
-            question={question}
-            option={option}
-            quizHandler={quizHandler}
-            previousQuestion={previousQuestion}
-            btnSubmit={submit}
-            submitQuiz={submitQuiz}
-          />
-        )}
-        {screen === "ScoreCard" && <ScoreCard score={score} reQuiz={reQuiz} />}
-        {screen === "Start" && <Initially start={start}/>}
+        <FbFeed profileDetails={profileDetails} images={images}/>
       </Container>
     </div>
   );
---
+++ src/views/Fb Feed/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "../Fb Feed/index.css";
+import { Row, Col } from "react-bootstrap";
+import FbImageLibrary from "react-fb-image-grid";
+import { BiWorld } from "react-icons/bi";
+// import profile from "../../Assets/Images/Photo.jpg";
+export default function FbFeed(props) {
+  console.log(props.profileDetails.userImages);
+  return (
+    <>
+      <Row
+        style={{
+          backgroundColor: "#eee",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Col xl={6} style={{}}>
+          <div className="feed-box">
+            <Row>
+              <Col
+                style={{
+                  paddingTop: "15px",
+                  paddingBottom: "15px",
+                  paddingLeft: "20px",
+                }}
+              >
+                <div className="feed-head">
+                  <div className="profile">
+                    <img
+                      src={props.profileDetails.profileImage}
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="profile-box">
+                    <div className="profile-name">
+                      <span>{props.profileDetails.profileName}</span>
+                    </div>
+                    <div className="profile-date">
+                      <span>{props.profileDetails.date}</span>
+                      <BiWorld
+                        color="gray"
+                        size={18}
+                        style={{
+                          marginTop: "5px",
+                          marginLeft: "7px",
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div className="profile-caption">
+                  <p>{props.profileDetails.userCaption}</p>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col >
+                <div className="user-images">
+                  <FbImageLibrary images={props.images} width={'100'} />
+                </div>
+              </Col>
+            </Row>
+          </div>
+
+          {/* / */}
+        </Col>
+      </Row>
+    </>
+  );
+}
